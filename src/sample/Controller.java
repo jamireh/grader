9f98c0f670c56e60184fc661f6a0c81f6940b478
Jon Amireh <jon.amireh@gmail.com>
2015-03-07 13:13:07 -0800

    Reacts to input now

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,10 +2,13 @@ package sample;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -15,17 +18,40 @@ public class Controller implements Initializable {
     ComboBox cbCatParent;
     @FXML
     TextField tfCatName;
+    @FXML
+    Button bAdd;
+    @FXML
+    ComboBox cbWeights;
+    @FXML
+    TextField tfWeight;
 
 
     @Override
     public void initialize(URL location, ResourceBundle resources)
     {
-        ObservableList<String> options =
+        ObservableList<String> parentOptions =
                 FXCollections.observableArrayList(
                         "CPE 309"
                 );
-        cbCatParent.setPromptText(options.get(0));
-        cbCatParent.setItems(options);
+        cbCatParent.setValue(parentOptions.get(0));
+        cbCatParent.setItems(parentOptions);
+        ObservableList<String> weightOptions =
+                FXCollections.observableArrayList(
+                        "Evenly within the category",
+                        "According to raw scores"
+                );
+        cbWeights.setValue(weightOptions.get(0));
+        cbWeights.setItems(weightOptions);
 
+    }
+
+    public void handleButtonPress(ActionEvent actionEvent)
+    {
+        System.out.println("Name: " + tfCatName.getText());
+        System.out.println("Parent: " + cbCatParent.getValue());
+        System.out.println("Weight: " + tfWeight.getText());
+        System.out.println("Weight Behavior: " + cbWeights.getValue());
+        Stage stage = (Stage) bAdd.getScene().getWindow();
+        stage.close();
     }
 }
