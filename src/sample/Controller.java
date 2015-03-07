8089f4313da2023960c97328351201fdb772b893
Jon Amireh <jon.amireh@gmail.com>
2015-03-07 14:36:28 -0800

    Assignments finished

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -5,25 +5,22 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
-    @FXML
-    ComboBox cbCatParent;
-    @FXML
-    TextField tfCatName;
-    @FXML
-    Button bAdd;
-    @FXML
-    ComboBox cbWeights;
-    @FXML
-    TextField tfWeight;
+    @FXML ComboBox cbCatParent;
+    @FXML TextField tfCatName;
+    @FXML TextField tfWeight;
+    @FXML TextField tfRawScore;
+    @FXML DatePicker dpDueDate;
+    @FXML CheckBox chbHandin;
+    @FXML Label lHandin;
+    @FXML TextField tfHandin;
+    @FXML Button bAdd;
 
 
     @Override
@@ -35,23 +32,24 @@ public class Controller implements Initializable {
                 );
         cbCatParent.setValue(parentOptions.get(0));
         cbCatParent.setItems(parentOptions);
-        ObservableList<String> weightOptions =
-                FXCollections.observableArrayList(
-                        "Evenly within the category",
-                        "According to raw scores"
-                );
-        cbWeights.setValue(weightOptions.get(0));
-        cbWeights.setItems(weightOptions);
 
     }
 
-    public void handleButtonPress(ActionEvent actionEvent)
+    public void onAddButtonPress(ActionEvent actionEvent)
     {
         System.out.println("Name: " + tfCatName.getText());
         System.out.println("Parent: " + cbCatParent.getValue());
+        System.out.println("Raw Scores: " + tfRawScore.getText());
         System.out.println("Weight: " + tfWeight.getText());
-        System.out.println("Weight Behavior: " + cbWeights.getValue());
+        System.out.println("Due Date: " + dpDueDate.getValue());
+        System.out.println("Handin: " + chbHandin.isSelected() + (chbHandin.isSelected() ? " directory: " + tfHandin.getText() : ""));
         Stage stage = (Stage) bAdd.getScene().getWindow();
         stage.close();
+    }
+
+    public void onHandinChecked(ActionEvent event)
+    {
+        lHandin.setDisable(!chbHandin.isSelected());
+        tfHandin.setDisable(!chbHandin.isSelected());
     }
 }
