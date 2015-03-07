4beadc407535e66bab4947223d34b182806d8a4d
Jon Amireh <jon.amireh@gmail.com>
2015-03-07 12:51:30 -0800

    category gui complete

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,31 @@
 package sample;
 
-public class Controller {
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    @FXML
+    ComboBox cbCatParent;
+    @FXML
+    TextField tfCatName;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources)
+    {
+        ObservableList<String> options =
+                FXCollections.observableArrayList(
+                        "CPE 309"
+                );
+        cbCatParent.setPromptText(options.get(0));
+        cbCatParent.setItems(options);
+
+    }
 }
