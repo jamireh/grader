a35d6a8f7d92983da6c1052233077aea9d6555d0
bleischt <bleischt@hotmail.com>
2015-04-13 01:31:26 +0200

    Added ActionEvent methods for editing divisions text fields in the GradeScheme menu. Commented out is the call to the actual update divisions method of GradeScheme.

--- src/grader/controller/GradeSchemeController.java
+++ src/grader/controller/GradeSchemeController.java
@@ -3,7 +3,10 @@ package grader.controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ColorPicker;
 import javafx.stage.Stage;
+import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
 
 public class GradeSchemeController
 {
@@ -14,5 +17,17 @@ public class GradeSchemeController
         System.out.println("Done button clicked");
         Stage stage = (Stage) bDone.getScene().getWindow();
         stage.close();
+    }
+
+    public void onTextFieldEdited(ActionEvent event)
+    {
+        //MainController.course.sections.get(0).gradeScheme.updateDivisions();
+        TextField textField = (TextField) event.getSource();
+        System.out.println("Updated information: " + textField.getCharacters());
+    }
+
+    public void onColorChosen(ActionEvent event)
+    {
+        System.out.println("Color updated: " + ((ColorPicker)event.getSource()).getValue());
     }
 }
