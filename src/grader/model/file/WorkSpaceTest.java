35a869d8e691ec30d152a82d6774a38bda2b5784
Gregory Davis <gpdavis@calpoly.edu>
2015-04-26 16:41:38 -0700

    workspace test

---
+++ src/grader/model/file/WorkSpaceTest.java
@@ -0,0 +1,122 @@
+package grader.model.file;
+
+import static org.junit.Assert.*;
+
+/**
+ * The WorkSpaceTest class is the companion testing class for the grader
+ * WorkSpace. It implements the following module test plan:
+ *									                                       <pre>
+ *    Phase 1: Unit test the constructor.
+ *
+ *    Phase 2: Unit test the sidebarSelect method and the getters for the
+ *             scope.
+ *
+ *    Phase 3: Unit test getters for scope related data such as getStudents,
+ *             getAssignmentTree, and getGradeScheme.
+ *
+ *    Phase 4: Unit test updating the grade scheme.
+ *
+ *    Phase 5: Unit test updating, reverting, and saving grades.
+ *
+ *    Phase 6: Unit test undo and redo functionality, including canUndo/canRedo
+ *             along with getLatestChange and getLatestUndo.
+ *
+ *    Phase 7: Repeat phases 1 through 6.
+ *	                                       								 </pre>
+ */
+public class WorkSpaceTest {
+    @org.junit.Test
+    public void testGetGradebook() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testGetCourse() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testGetSection() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testGetGroup() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testGetStudents() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testGetAssignmentTree() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testGetScores() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testGetGradeScheme() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testSidebarSelect() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testUpdateGrade() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testRevertGrades() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testSaveGrades() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testUpdateGradeScheme() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testCanUndo() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testCanRedo() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testUndo() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testRedo() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testGetLatestChange() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testGetLatestUndo() throws Exception {
+
+    }
+}
