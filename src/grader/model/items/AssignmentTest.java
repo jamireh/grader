877da342e3455d631a903cbc92263c218acede2f
Jon Amireh <jon.amireh@gmail.com>
2015-04-26 19:02:43 -0700

    test plans added

--- src/grader/model/items/AssignmentTest.java
+++ src/grader/model/items/AssignmentTest.java
@@ -3,7 +3,21 @@ package grader.model.items;
 import grader.model.errors.PercentageFormatException;
 import grader.model.errors.RawScoreFormatException;
 import org.junit.Test;
-
+/**
+ * The AssignmentTest class is the companion testing class for the grader
+ * Assignment. It implements the following module test plan:
+ *									                                       <pre>
+ *    Phase 1: Unit test the constructor.
+ *
+ *    Phase 2: Unit test the constructor with invalid values.
+ *
+ *    Phase 3: Unit test adjustPointValue
+ *
+ *    Phase 4: Unit test getPoints
+ *
+ *    Phase 3: Repeat phases 1 through 4.
+ *	                                       								 </pre>
+ */
 public class AssignmentTest
 {
 
--- src/grader/model/items/CategoryTest.java
+++ src/grader/model/items/CategoryTest.java
@@ -2,7 +2,17 @@ package grader.model.items;
 
 import grader.model.errors.PercentageFormatException;
 import org.junit.Test;
-
+/**
+ * The CategoryTest class is the companion testing class for the grader
+ * Category. It implements the following module test plan:
+ *									                                       <pre>
+ *    Phase 1: Unit test the constructor.
+ *
+ *    Phase 2: Unit test the constructor with invalid values.
+ *
+ *    Phase 3: Repeat phases 1 through 2.
+ *	                                       								 </pre>
+ */
 public class CategoryTest
 {
     @Test(expected = PercentageFormatException.class)
