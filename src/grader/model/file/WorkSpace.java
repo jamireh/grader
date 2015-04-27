68d28f4e9337725eeb2fef29693fb1619ef3d49b
Gregory Davis <gpdavis@calpoly.edu>
2015-04-26 17:02:38 -0700

    oops

--- src/grader/model/file/WorkSpace.java
+++ src/grader/model/file/WorkSpace.java
@@ -33,8 +33,8 @@ public class WorkSpace extends Observable {
     */
    public static final WorkSpace instance = new WorkSpace();
    static {
-      setChanged();
-      notifyObservers();
+      instance.setChanged();
+      instance.notifyObservers();
    }
 
    /**
