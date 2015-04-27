d159c52d6cbc6c71ccedb07b6a446b16d8b9cf9c
Quan Tran <kiettran@Kiets-MacBook-Pro.local>
2015-04-26 19:20:29 -0700

    stats view

--- src/grader/view/StatisticsBar.java
+++ src/grader/view/StatisticsBar.java
@@ -2,19 +2,34 @@ package grader.view;
 
 import grader.model.gradebook.stats.Statistics;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javafx.collections.ObservableList;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 /**
  * The view for the Statistics bar.
  */
 public class StatisticsBar extends TableView<Statistics> {
+    List<TableColumn<Statistics, Double>> columns;
+
     /**
      * Constructs a new StatisticsBar for the given data.
      */
     public StatisticsBar(ObservableList<Statistics> list) {
         super(list);
-        System.out.println("stats bar constructed");
-        setVisible(true);
+
+        columns = new ArrayList<TableColumn<Statistics, Double>>();
+        for (Statistics stat : list) {
+            TableColumn<Statistics, Double> current =
+                    new TableColumn<Statistics, Double>();
+
+            current.setCellValueFactory(new PropertyValueFactory("pMax"));
+        }
+
+        getColumns().setAll(columns);
     }
 }
