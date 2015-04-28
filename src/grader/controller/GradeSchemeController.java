f69e7d34f3ed69b4a22f2c32607d5037ca6dcfe3
bleischt <bleischt@hotmail.com>
2015-04-28 04:00:56 +0200

    Merge branch 'master' of https://github.com/jamireh/grader
    Conflicts:
    	src/grader/model/gradebook/GradeScheme.java

--- src/grader/controller/GradeSchemeController.java
+++ src/grader/controller/GradeSchemeController.java
@@ -1,5 +1,6 @@
 package grader.controller;
 
+import grader.model.gradebook.GradeRange;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -7,6 +8,8 @@ import javafx.scene.control.ColorPicker;
 import javafx.stage.Stage;
 import javafx.scene.control.TextField;
 import javafx.scene.paint.Color;
+import java.util.Collection;
+import java.util.List;
 
 /**
  * GradeSchemeController is the controller for the 'GradeScheme' menu option.
@@ -17,7 +20,66 @@ import javafx.scene.paint.Color;
  */
 public class GradeSchemeController
 {
+    public TextField A_Plus_High;
+    public TextField A_Plus_Low;
+    public TextField A_High;
+    public TextField A_Low;
+    public TextField A_Minus_High;
+    public TextField A_Minus_Low;
+    public TextField B_Plus_High;
+    public TextField B_Plus_Low;
+    public TextField B_High;
+    public TextField B_Low;
+    public TextField B_Minus_High;
+    public TextField B_Minus_Low;
+    public TextField C_Plus_High;
+    public TextField C_Plus_Low;
+    public TextField C_High;
+    public TextField C_Low;
+    public TextField C_Minus_High;
+    public TextField C_Minus_Low;
+    public TextField D_Plus_High;
+    public TextField D_Plus_Low;
+    public TextField D_High;
+    public TextField D_Low;
+    public TextField D_Minus_High;
+    public TextField D_Minus_Low;
+    public TextField F_High;
+    public TextField F_Low;
     @FXML Button bDone;
+
+    @FXML
+    private void initialize() {
+        //List<GradeRange> ranges = MainController.course.sections.get(0).gradeScheme.ranges;
+        A_Plus_High.setText("100");
+        A_Plus_High.setDisable(true);
+        A_Plus_Low.setText("97.0");
+        A_High.setText("97.0");
+        A_Low.setText("93.0");
+        A_Minus_High.setText("93.0");
+        A_Minus_Low.setText("90.0");
+        B_Plus_High.setText("90.0");
+        B_Plus_Low.setText("87.0");
+        B_High.setText("87.0");
+        B_Low.setText("83.0");
+        B_Minus_High.setText("83.0");
+        B_Minus_Low.setText("80.0");
+        C_Plus_High.setText("80.0");
+        C_Plus_Low.setText("77.0");
+        C_High.setText("77.0");
+        C_Low.setText("73.0");
+        C_Minus_High.setText("73.0");
+        C_Minus_Low.setText("70.0");
+        D_Plus_High.setText("70.0");
+        D_Plus_Low.setText("67.0");
+        D_High.setText("67.0");
+        D_Low.setText("63.0");
+        D_Minus_High.setText("63.0");
+        D_Minus_Low.setText("60.0");
+        F_High.setText("60.0");
+        F_Low.setText("0");
+        F_Low.setDisable(true);
+}
 
     /**
      * Closes the GradeScheme menu GUI upon click of the "Done" button.
@@ -30,10 +92,10 @@ public class GradeSchemeController
         stage.close();
     }
 
+
+
     /**
-     * TODO - Write method to run through the existing GradeRanges and check
-     * their validity (Overlap). Result comes back from onTextFieldEdited and
-     * throw an exception if it's not correct.
+     * TODO - Once the prototype is back up to creating concrete Sections, display data from the model in the view
      */
 
     /**
---
+++ src/grader/model/gradebook/GradeRangeTest.java
@@ -0,0 +1,72 @@
+package grader.model.gradebook;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GradeRangeTest {
+
+    /**
+     * The GradeRangeTest class is the companion testing class for the grader
+     * GradeRange. It implements the following module test plan:
+     *									                                       <pre>
+     *    Phase 1: Unit test the constructor.
+     *
+     *    Phase 2: Unit test the constructor with invalid values.
+     *
+     *    Phase 3: Unit test all accessor methods.
+     *
+     *    Phase 4: Unit test all mutator methods.
+     *
+     *    Phase 5: Unit test toString.
+     *
+     *    Phase 6: Repeat phases 1 through 5.
+     *	                                       								 </pre>
+     *	  @author Tobias Bleisch
+     */
+
+    @Test
+    public void testValidGradeRange() throws Exception {
+
+    }
+
+    @Test
+    public void testInvalidGradeRange() throws Exception {
+
+    }
+
+    @Test
+    public void testGetLowerBound() throws Exception {
+
+    }
+
+    @Test
+    public void testSetLowerBound() throws Exception {
+
+    }
+
+    @Test
+    public void testGetLetterGrade() throws Exception {
+
+    }
+
+    @Test
+    public void testSetLetterGrade() throws Exception {
+
+    }
+
+    @Test
+    public void testGetColor() throws Exception {
+
+    }
+
+    @Test
+    public void testSetColor() throws Exception {
+
+    }
+
+    @Test
+    public void testToString() throws Exception {
+
+    }
+}
